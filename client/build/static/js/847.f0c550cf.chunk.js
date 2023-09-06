959e917f2304aa4f2d6f8d7cdfbd496b2d684070
Dipanjali Gubreley <dipanjali180302@gmail.com>
2023-09-07 00:56:20 +0530

    Add changes

---
+++ client/build/static/js/847.f0c550cf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[847],{7847:function(e,n,r){r.r(n);var l=r(3523),t=r(890),a=r(7689),i=r(184);n.default=function(){var e=(0,a.lk)();return console.log(e),(0,i.jsx)(l.Z,{style:{marginLeft:250},children:(0,i.jsx)(t.Z,{variant:"h4",children:"There was error loading this page"})})}}}]);
+//# sourceMappingURL=847.f0c550cf.chunk.js.map
